734bb288286deda8eb58c261659db6bdbe65ec7e
Akkordion <akkordion.palych@gmail.com>
2023-01-23 18:54:13 +0300

    Started a new app to search for recepies Made a main screen with a grid full of dummy data categories

---
+++ android/app/src/main/kotlin/com/example/cusinatra/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cusinatra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/categories_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import './category_item.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cusinatra'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map(
+              (categoryData) => CategoryItem(
+                categoryData.title,
+                categoryData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
