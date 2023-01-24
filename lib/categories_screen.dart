2c4fc5889288335a8d8a1e61687d7f5eee2668f9
Akkordion <akkordion.palych@gmail.com>
2023-01-24 16:35:27 +0300

    Screen for meal of chosen category
    Vrapped category containers with gesture detectors (InkWell). Created scheme of category recepies screen and linked it with categories on the main screen.

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -15,6 +15,7 @@ class CategoriesScreen extends StatelessWidget {
         children: DUMMY_CATEGORIES
             .map(
               (categoryData) => CategoryItem(
+                categoryData.id,
                 categoryData.title,
                 categoryData.color,
               ),
